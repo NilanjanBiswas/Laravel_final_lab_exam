85a9c998e45fbc4a2f28969c8129768c23f1a8ba
Nilanjan Biswas <nilanjanmotoxforce@gmail.com>
2020-09-08 22:53:42 +0600

    M1 T1 Admin CRUD done

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/login', 'LoginController@index');
+
+//admin 
+Route::get('/admin','AdminController@index');
+Route::get('/admin/users','AdminController@users');
+Route::get('/admin/viewprofile/{username}','AdminController@viewProfile')->name('admin.viewprofile');
+Route::post('/admin/viewprofile/{username}','AdminController@viewProfilePost')->name('admin.viewprofile');
+Route::get('/admin/register','AdminController@register');
+Route::post('/admin/register','AdminController@registerPost');
+
+//Employee 
+Route::get('/employee','EmployeeController@index');
+Route::get('/employee/users','EmployeeController@users');
